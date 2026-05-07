aac324569deecce6445fc559e1c9d97ea5369d7c
Eshita-123 <eshita@educerns.org>
2026-05-07 22:21:11 +0530

    provider

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,8 +1,8 @@
 "use client";
-import { QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from '../context/AuthContext';
-import { getQueryClient } from '../lib/queryClient';
-import { useRef } from 'react';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "../context/AuthContext";
+import { getQueryClient } from "../lib/queryClient";
+import { useRef } from "react";
 
 export default function Providers({ children }) {
   const queryClientRef = useRef();
